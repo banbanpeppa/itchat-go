608fa3f843b07315936ca6fdba524c1d387f2752
banban <1262098118@qq.com>
2019-08-28 16:20:10 +0800

    使用dep管理golang项目

--- test/itchat_final_test.go
+++ test/itchat_final_test.go
@@ -67,13 +67,18 @@ func TestGetAllGroups(t *testing.T) {
 	var c wx.LoginCallback
 	util.Load(&c, "wx_cache/itchat.pkl")
 
-	contraces, err := service.GetAllContact(c.LoginMap)
+	contracts, err := service.GetAllContact(c.LoginMap)
 	if err != nil {
 		log.Println(err)
 	}
 
-	fmt.Printf("成功获取 %d个 联系人信息,开始整理群组信息...\n", len(contraces))
-	groupMap := service.MapGroupInfo(contraces)
+	fmt.Printf("成功获取 %d个 联系人信息,开始整理群组信息...\n", len(contracts))
+	for _, c := range contracts {
+		if c.RemarkName == "钟贤鹏" {
+			fmt.Println(c)
+		}
+	}
+	groupMap := service.MapGroupInfo(contracts)
 	groupSize := 0
 	for _, v := range groupMap {
 		groupSize += len(v)
@@ -88,24 +93,28 @@ func TestGetAllGroups(t *testing.T) {
 }
 
 func TestSendMessage(t *testing.T) {
-	var c wx.LoginCallback
-	util.Load(&c, "wx_cache/itchat.pkl")
-	wxSendMsg := model.WxSendMsg{}
-	wxSendMsg.Type = 1
-	wxSendMsg.Content = "我是banbanpeppa编写的微信机器人，我已帮你通知我的主人，请您稍等片刻，他会跟您联系"
-	wxSendMsg.FromUserName = c.LoginMap.SelfUserName
-	// wxSendMsg.ToUserName = "@1ee21d885123c7ffc7738a5ba30599c9dd5a0c8b0363473f97d4a9fce333eb24"
-	wxSendMsg.ToUserName = "@069c9a42daa16199fc05a607afb2bf4de969aca515472329696bf2223362ff32"
-	wxSendMsg.LocalID = fmt.Sprintf("%d", time.Now().Unix())
-	wxSendMsg.ClientMsgId = wxSendMsg.LocalID
+	for {
+		var c wx.LoginCallback
+		util.Load(&c, "wx_cache/itchat.pkl")
+		wxSendMsg := model.WxSendMsg{}
+		wxSendMsg.Type = 1
+		wxSendMsg.Content = "回你回你🙂🦉🐺🦈"
+		wxSendMsg.FromUserName = c.LoginMap.SelfUserName
+		// wxSendMsg.ToUserName = "@1ee21d885123c7ffc7738a5ba30599c9dd5a0c8b0363473f97d4a9fce333eb24"
+		wxSendMsg.ToUserName = "@6a738dd6b14b14c3122e5a0a6a5377641ca9176d0dca81cebe7126478b4083f8"
+		wxSendMsg.LocalID = fmt.Sprintf("%d", time.Now().Unix())
+		wxSendMsg.ClientMsgId = wxSendMsg.LocalID
 
-	/* 加点延时，避免消息次序混乱，同时避免微信侦察到机器人 */
-	time.Sleep(time.Second)
+		/* 加点延时，避免消息次序混乱，同时避免微信侦察到机器人 */
+		time.Sleep(time.Second)
 
-	err := service.SendMsg(c.LoginMap, wxSendMsg)
-	if err != nil {
-		fmt.Println(err)
+		err := service.SendMsg(c.LoginMap, wxSendMsg)
+		if err != nil {
+			fmt.Println(err)
+		}
+		time.Sleep(20 * time.Second)
 	}
+
 }
 
 func TestGetLoginQrcode(t *testing.T) {
